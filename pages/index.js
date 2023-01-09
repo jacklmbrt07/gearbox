349627bcb76eb060f7cec5fbb1c7070cd3e8b8a6
jacklmbrt07 <jacklmbrt07@gmail.com>
2023-01-08 16:40:53 -0800

    added footer banner component

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,19 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 const Home = ({ products, bannerData }) => {
   return (
     <div>
-      <HeroBanner heroBanner={bannerData.length && bannerData[0] } />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   );
 };
