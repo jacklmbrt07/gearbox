1153f998332523c8540e7ce7cb51876f21168100
jacklmbrt07 <jacklmbrt07@gmail.com>
2023-01-08 19:41:34 -0800

    added cart logic

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
+import Cart from "./Cart";
+import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className="navbar-container">
-      <p>
-        <Link href={"/"}>
-          <button type="button" className="cart-icon" onClick="">
-            <AiOutlineShopping />
-            <span className="cart-item-qty">3</span>
-          </button>
-        </Link>
+      <p className="logo">
+        <Link href="/">Home</Link>
       </p>
-    </div>
-  );
-};
 
-export default Navbar;
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+
+      {showCart && <Cart />}
+    </div>
+  )
+}
+
+export default Navbar
